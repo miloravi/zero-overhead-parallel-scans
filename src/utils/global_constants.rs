a0ada8ee31db54157b3138f4a9aa18ccbb191bf8
miloravi <57906122+miloravi@users.noreply.github.com>
2025-03-27 12:28:11 +0100

    Added THREAD_COUNTS const

--- src/utils/global_constants.rs
+++ src/utils/global_constants.rs
@@ -6,6 +6,7 @@
 
 //// Intel 12900
 // pub const AFFINITY_MAPPING: [usize; 24] = [0, 2, 4, 6, 8, 10, 12, 14, 16, 17, 18, 19, 20, 21, 22, 23, 1, 3, 5, 7, 9, 11, 13, 15];
+// pub const THREAD_COUNTS: [usize; 10] = [1, 2, 4, 6, 8, 10, 12, 16, 20, 24];
 
 // Intel i7 - 10750H
 pub const AFFINITY_MAPPING: [usize; 12] = [0, 2, 4, 6, 8, 10, 1, 3, 5, 7, 9, 11];
